e42a7913d6c41c6d8a9447213bb5e09cb276df0f
ViNguyen <vinguyen@magrabbit.com>
2012-07-12 14:06:21 +0700

    add new file

---
+++ Mylocations/exam.m
@@ -0,0 +1,13 @@
+//
+//  exam.m
+//  Mylocations
+//
+//  Created by vinguyen on 7/12/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "exam.h"
+
+@implementation exam
+
+@end
